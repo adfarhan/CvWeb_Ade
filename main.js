8d6c4f99e6c15a54d1973f21910ca6cd9750cf91
adfarhan <149446159+adfarhan@users.noreply.github.com>
2024-07-11 13:18:47 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const typed = new typed('multiple-text',{
+    string: ['Web Developer','Web Designer', 'Student'],
+    typeSpeed : 70,
+    backSpeed : 70,
+    backDelay: 1000,
+    loop:true,
+});
