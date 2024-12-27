acaaa5e31fd2077fbbbd8e97844f55b295f523be
adfarhan <149446159+adfarhan@users.noreply.github.com>
2024-12-27 19:55:42 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-const typed = new typed('multiple-text',{
-    string: ['Web Developer','Web Designer', 'Student'],
-    typeSpeed : 70,
-    backSpeed : 70,
-    backDelay: 1000,
-    loop:true,
+const menu = document.getElementById('menu');
+const navbarNav = document.querySelector('.navbar-nav');
+
+// Tampilkan atau sembunyikan menu saat ikon diklik
+menu.addEventListener('click', (event) => {
+  event.stopPropagation(); // Mencegah klik dari bubbling ke document
+  navbarNav.classList.toggle('active');
+});
+
+// Tutup menu jika klik di luar menu
+document.addEventListener('click', (event) => {
+  if (!navbarNav.contains(event.target) && !menu.contains(event.target)) {
+    navbarNav.classList.remove('active');
+  }
 });
